5f4d922b0e8d1101446580c07478969ae6933f8b
Pete Ngai <sendccme55@gmail.com>
2020-06-10 20:48:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,17 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def position_taken? (board, index)
+
+  current_board_player = board[index]
+
+  if (current_board_player == " ") || (current_board_player == "")
+    false
+  elsif current_board_player == "X"
+    true
+  elsif current_board_player == "O"
+    true
+  else false
+  end
+end
